e2e26cd16927667da3af5067a9bf986c3d911239
Matthew Schwarz <schwarz1016@gmail.com>
2018-02-08 15:12:03 -0500

    added dimens.xml

--- app/src/main/java/com/example/matthewschwarz/tipcounter/TipScreen.java
+++ app/src/main/java/com/example/matthewschwarz/tipcounter/TipScreen.java
@@ -10,6 +10,8 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class TipScreen extends AppCompatActivity {
 
@@ -50,6 +52,7 @@ public class TipScreen extends AppCompatActivity {
         //make these not shown or not clickable yet
         otherAmount.setVisibility(View.INVISIBLE);
         calculateButton.setEnabled(false);
+        tTotal.setText(null); fTotal.setText(null); indivTotal.setText(null);
 
         //numPeople.setOnKeyListener(mKeyListener);
 
@@ -73,13 +76,24 @@ public class TipScreen extends AppCompatActivity {
                 double total = Double.parseDouble(billTotal.getText().toString());
                 double tip = Double.parseDouble(tipSelected);
                 int people = Integer.parseInt(numPeople.getText().toString());
-                double tipAmount = total * (tip * .01);
-                double totalWithTip = total + tipAmount;
-                double totalEachPays = totalWithTip / people;
-                tTotal.setText(String.valueOf(tipAmount));
-                fTotal.setText(String.valueOf(totalWithTip));
-                indivTotal.setText(String.valueOf(totalEachPays));
-
+                if(people < 1){
+                    showErrorAlert((getString(R.string.peopleError)), R.id.num_people);
+                }else if(tip < 1){
+                    showErrorAlert((getString(R.string.tipError)), R.id.other_tip_input);
+                }else if (total < 1){
+                    showErrorAlert((getString(R.string.billError)), R.id.bill_total);
+                }else{
+                    double tipAmount = total * (tip * .01);
+                    double totalWithTip = total + tipAmount;
+                    double totalEachPays = totalWithTip / people;
+                    //set the strings to the tip values
+                    String tipTotal = getString(R.string.tipAmnt, String.valueOf(tipAmount));
+                    tTotal.setText(tipTotal);
+                    String fullTotal = getString(R.string.totalAmnt, String.valueOf(totalWithTip));
+                    fTotal.setText(fullTotal);
+                    String splitTotal = getString(R.string.splitAmnt, String.valueOf(totalEachPays));
+                    indivTotal.setText(splitTotal);
+                }
             }
         });
 
@@ -89,21 +103,24 @@ public class TipScreen extends AppCompatActivity {
 
                 switch(v.getId()){
                     case R.id.num_people:
-                        if(numPeople.getText().length() > 0 && billTotal.getText().length() > 0){
+                        if(numPeople.getText().length() > 0 && billTotal.getText().length() > 0
+                                && radioGroup.getCheckedRadioButtonId() != -1){
                             calculateButton.setEnabled(true);
                         }else{
                             calculateButton.setEnabled(false);
                         }
                         break;
                     case R.id.bill_total:
-                        if(numPeople.getText().length() > 0 && billTotal.getText().length() > 0){
+                        if(numPeople.getText().length() > 0 && billTotal.getText().length() > 0
+                                && radioGroup.getCheckedRadioButtonId() != -1){
                             calculateButton.setEnabled(true);
                         }else{
                             calculateButton.setEnabled(false);
                         }
                         break;
                     case R.id.other_tip_input:
-                        if(numPeople.getText().length() > 0 && billTotal.getText().length() > 0 && otherAmount.getText().length() > 0){
+                        if(numPeople.getText().length() > 0 && billTotal.getText().length() > 0
+                                && otherAmount.getText().length() > 0 && radioGroup.getCheckedRadioButtonId() != -1){
                             //store the custom tip in tipSelected string to be used when calculating
                             tipSelected = otherAmount.getText().toString();
                             calculateButton.setEnabled(true);
@@ -129,14 +146,23 @@ public class TipScreen extends AppCompatActivity {
                     otherAmount.setVisibility(View.INVISIBLE);
                     tipSelected = "15";
                     Toast.makeText(getApplicationContext(), "15% tip", Toast.LENGTH_SHORT).show();
+                    if(numPeople.getText().length() > 0 && billTotal.getText().length() > 0){
+                        calculateButton.setEnabled(true);
+                    }
                 }else if(rb == radio18){
                     otherAmount.setVisibility(View.INVISIBLE);
                     tipSelected = "18";
                     Toast.makeText(getApplicationContext(), "18% tip", Toast.LENGTH_SHORT).show();
+                    if(numPeople.getText().length() > 0 && billTotal.getText().length() > 0){
+                        calculateButton.setEnabled(true);
+                    }
                 }else if(rb == radio20){
                     otherAmount.setVisibility(View.INVISIBLE);
                     tipSelected = "20";
                     Toast.makeText(getApplicationContext(), "20% tip", Toast.LENGTH_SHORT).show();
+                    if(numPeople.getText().length() > 0 && billTotal.getText().length() > 0){
+                        calculateButton.setEnabled(true);
+                    }
                 }else{
                     otherAmount.setVisibility(View.VISIBLE);
                     Toast.makeText(getApplicationContext(), "enter tip amount", Toast.LENGTH_SHORT).show();
@@ -145,10 +171,40 @@ public class TipScreen extends AppCompatActivity {
             }
         });
 
+    }
 
+    private void showErrorAlert(String errorMessage, final int fieldId) {
+        new AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(errorMessage)
+                .setNeutralButton("Close",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                findViewById(fieldId).requestFocus();
+                            }
+                        }).show();
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(getString(R.string.peopleKey), numPeople.getText().toString());
+        savedInstanceState.putString(getString(R.string.tipKey), tipSelected);
+        savedInstanceState.putString(getString(R.string.totalKey), billTotal.getText().toString());
 
+    }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        numPeople.setText(savedInstanceState.getString(getString(R.string.peopleKey)));
+        billTotal.setText(savedInstanceState.getString(getString(R.string.totalKey)));
+        tipSelected = savedInstanceState.getString(getString(R.string.tipKey));
+        if(numPeople.getText().length() > 0 && billTotal.getText().length() > 0 && tipSelected.length() > 0){
+            calculateButton.setEnabled(true);
+        }
 
 
     }
